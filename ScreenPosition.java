3cbc6a81fb7a893a30ce1f3c20382b1124f0ef6e
Mason <kriptolego@gmail.com>
2020-06-06 21:39:23 -0400

    Create ScreenPosition.java

---
+++ ScreenPosition.java
@@ -0,0 +1,58 @@
+// I have reason to believe the smooth dragging wont work without this.
+// Created by Eric Golde, fix from caterpillow#3310
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.ScaledResolution;
+
+public class ScreenPosition {
+
+    private static final Minecraft mc = Minecraft.getMinecraft();
+
+    private int x, y;
+
+    public ScreenPosition(double x, double y) {
+        setRelative(x, y);
+    }
+
+    public ScreenPosition(int x, int y) {
+        setAbsolute(x, y);
+    }
+
+    public static ScreenPosition fromRelativePosition(double x, double y) {
+        return new ScreenPosition(x, y);
+    }
+
+    public static ScreenPosition fromAbsolute(int x, int y) {
+        return new ScreenPosition(x, y);
+    }
+
+    public int getAbsoluteX() {
+        return x;
+    }
+
+    public int getAbsoluteY() {
+        return y;
+    }
+
+    public double getRelativeX() {
+        ScaledResolution sr = new ScaledResolution(mc);
+        return x / sr.getScaledWidth_double();
+    }
+
+    public double getRelativeY() {
+        ScaledResolution sr = new ScaledResolution(mc);
+        return y / sr.getScaledHeight_double();
+    }
+
+    public void setAbsolute(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public void setRelative(double x, double y) {
+        ScaledResolution sr = new ScaledResolution(mc);
+        this.x = (int) (sr.getScaledWidth() / x);
+        this.y = (int) (sr.getScaledHeight() / y);
+    }
+
+}
